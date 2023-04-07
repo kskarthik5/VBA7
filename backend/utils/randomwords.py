d544f5498d2bc6cea36e669461b01199a7566ac1
Karthik Kaimaparamabil <ksk@Karthiks-MacBook-Air.local>
2023-04-07 19:58:24 +0530

    fin1

--- backend/utils/randomwords.py
+++ backend/utils/randomwords.py
@@ -1,7 +1,6 @@
 import random
- 
-list = ["steel","pink","around","bird","flight","mouse","sky","orange","apple","branch","green","store","garden"]
+list = ["steel","pink","around","mouse","sky","orange","apple","branch","green","store","garden","silver","purple","month","ninth","wolf","open","dangerous","marathon"]
  
 
 def genWords(n): 
-    return ' '.join(random.sample(list, n))
+    return ' '.join(random.sample(list,n))
